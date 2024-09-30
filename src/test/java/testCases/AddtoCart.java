6d5db9cb4fe7f91791de61b3ed33f0594c3d996e
Jayanth <jayanthbonala72@gmail.com>
2024-09-30 23:26:54 +0530

    Check Product Availability Added_2

--- src/test/java/testCases/AddtoCart.java
+++ src/test/java/testCases/AddtoCart.java
@@ -47,18 +47,20 @@ public class AddtoCart {
     public void productAvailability()
     {
         driver.findElement(By.xpath("//summary[@aria-label='Search']//span")).click();
-        driver.findElement(By.id("Search-In-Modal")).sendKeys("Belted Jeans");
+        driver.findElement(By.id("Search-In-Modal")).sendKeys("Skinny Stretch Jean in Indigo");
         driver.findElement(By.xpath("//button[@class='search__button field__button']")).click();
-        WebElement prodlink = driver.findElement(By.xpath("//a[contains(text(),'Belted Jeans')]"));
+        WebElement prodlink = driver.findElement(By.xpath("//a[contains(text(),'Skinny Stretch Jean in Indigo')]"));
         prodlink.click();
-        WebElement addtocartbutton = driver.findElement(By.xpath("//button[@class='product-form__submit button button--full-width button--secondary']"));
-        if(addtocartbutton.isEnabled())
+        List<WebElement> soldOutIndicators = driver.findElements(By.xpath("//span[contains(text(),'Sold out')]"));
+        if(!soldOutIndicators.isEmpty())
         {
-            addtocartbutton.click();
+            Reporter.log("Item is sold out and not available",true);
+            Assert.assertTrue(soldOutIndicators.getFirst().isDisplayed(),"Sold out indicator should be displayed");
+            return;
         }
-        else {
-            Reporter.log("Item is out of stock");
-        }
+        WebElement addtocartbutton = driver.findElement(By.xpath("//button[@class='product-form__submit button button--full-width button--secondary']"));
+        Assert.assertTrue(addtocartbutton.isEnabled(),"Add to cart should be enabled");
+        addtocartbutton.click();
     }
 
 
