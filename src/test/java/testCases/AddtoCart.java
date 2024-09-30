cedca99201c1cc2ba31aedccd084e53d4e5a7f5f
Jayanth <jayanthbonala72@gmail.com>
2024-09-30 23:51:40 +0530

    Add Available Product to Cart

--- src/test/java/testCases/AddtoCart.java
+++ src/test/java/testCases/AddtoCart.java
@@ -63,11 +63,48 @@ public class AddtoCart {
         addtocartbutton.click();
     }
 
+    @Test
+    public void addAvailableProduct()
+    {
+        //getting inital cart size
+        driver.findElement(By.xpath("//a[@id='cart-icon-bubble']//*[name()='svg']")).click();
+        List<WebElement> initialitems = driver.findElements(By.xpath("//tr[@class='cart-item']"));
+
+        //searching and clicking on product
+        driver.findElement(By.xpath("//summary[@aria-label='Search']//span")).click();
+        driver.findElement(By.id("Search-In-Modal")).sendKeys("Belted Jeans");
+        driver.findElement(By.xpath("//button[@class='search__button field__button']")).click();
+        WebElement prodlink = driver.findElement(By.xpath("//a[contains(text(),'Belted Jeans')]"));
+        prodlink.click();
+        WebElement addtocartbutton = driver.findElement(By.xpath("//button[@class='product-form__submit button button--full-width button--secondary']"));
+        Assert.assertTrue(addtocartbutton.isEnabled(),"Add to cart should be enabled");
+        addtocartbutton.click();
+
+        //Verifying addtocart button enabled or not
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        WebElement viewcart = driver.findElement(By.xpath("//a[@class='button button--secondary button--full-width']"));
+        wait.until(ExpectedConditions.elementToBeClickable(viewcart));
+        String text = driver.findElement(By.xpath("//h2[@class='cart-notification__heading caption-large']")).getText().trim();
+        viewcart.click();
+
+        //getting the size of cart after adding product
+        driver.findElement(By.xpath("//a[@id='cart-icon-bubble']//*[name()='svg']")).click();
+        List<WebElement> items = driver.findElements(By.xpath("//tr[@class='cart-item']"));
+
+        //Confirming 'Item added to your cart' message appears or not
+        Assert.assertEquals(text,"Item added to your cart");
+
+        //Verifying cart's item count is incremented or not
+        Assert.assertTrue(items.size()>initialitems.size(),"Items not added");
+
+
+    }
+
 
 
    @AfterMethod
     public void closeBrowser()
     {
         driver.quit();
-    }
+   }
 }
